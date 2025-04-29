59c78b031402b8e27b7903e86fa18326d554bac1
pamelamichellga01 <pamelamichellga@ufps.edu.co>
2025-04-28 19:00:07 -0500

    clase listas

---
+++ PYTHON/Listas/listas1.py
@@ -0,0 +1,26 @@
+#Listas de edades
+
+edades =[16,17,20,22,23,25,26,28,30,50,50,50]
+
+print(f"Lista original{edades}")
+
+edades.append(35)
+
+# print(f"Lista append: {edades}")
+
+edades.insert(2,18)
+
+# print(f"Lista insert: {edades}")
+
+count=edades.count(50)
+
+# print(f"conteo: {count}")
+
+edades.remove(50)
+
+# print(f"Lista remove: {edades}")
+
+edades.pop()
+
+# print(f"Lista pop: {edades}")
+
